227e6146f07e755acb0f0fc3d8d9313d7023ddad
mikey <mikeysdigitalassets@gmail.com>
2024-06-12 12:45:33 -0500

    added hosted db credentials

--- server/server.js
+++ server/server.js
@@ -3,14 +3,29 @@ const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+require('dotenv').config();
+// local host postgres database 
+// const pool = new Pool({
+//     user:"postgres",
+//     host:"localhost",
+//     database:"worlds",
+//     password:"kali",
+//     port: 5432,
+// });
 
+
+// amazon rds hosted database
 const pool = new Pool({
-    user:"postgres",
-    host:"localhost",
-    database:"worlds",
-    password:"kali",
-    port: 5432,
-});
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: String(process.env.DB_PASSWORD),
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false // for self-signed certificates
+      }
+  });
+
 
 app.use(express.json());
 app.use(cors());
