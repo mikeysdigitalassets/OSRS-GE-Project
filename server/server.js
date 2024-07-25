f88e7587715b75e777e60706722985270a9b7746
mikey <mikeysdigitalassets@gmail.com>
2024-07-25 11:09:39 -0500

    deployment

--- server/server.js
+++ server/server.js
@@ -42,10 +42,10 @@ app.use(session({
     }
 }));
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 // app.use(cors({
