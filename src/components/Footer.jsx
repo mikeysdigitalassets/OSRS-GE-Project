3b39c14c9edfbbb371cb37c90053275f59733187
mikey <mikeysdigitalassets@gmail.com>
2024-07-25 11:20:26 -0500

    deployment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,6 @@ const Footer = () => {
     return (
         <img  style={{ height: '100px', width: '100px' }} src={`${process.env.PUBLIC_URL}/images/flower.png`}/>
     )
-}
+};
 
 export default Footer;
